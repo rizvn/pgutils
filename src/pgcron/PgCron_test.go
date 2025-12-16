ce25ad8bc0bfbb8d2526f6c798b825ca6ae2ae02
Riz <Riz>
2025-12-16 20:31:20 +0000

    run image from dockerfile

--- src/pgcron/PgCron_test.go
+++ src/pgcron/PgCron_test.go
@@ -2,14 +2,67 @@ package pgcron_test
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rizvn/pgutils/pgcron"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestPgCron(t *testing.T) {
-	config, err := pgxpool.ParseConfig("postgres://app_admin:app_admin@localhost:5432/app_db")
+	ctx := context.Background()
+
+	// define Postgres container request
+	req := testcontainers.ContainerRequest{
+		// Using custom Dockerfile to include pg_cron extension
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context:        "../../docker-compose/postgres",
+			Dockerfile:     "postgres-custom.Dockerfile",
+			BuildLogWriter: os.Stdout,
+			Tag:            "pg-test",
+			KeepImage:      true,
+		},
+		//Image:        "postgres:17-alpine",
+		ExposedPorts: []string{"5432/tcp"},
+		Env: map[string]string{
+			"POSTGRES_USER":     "app_admin",
+			"POSTGRES_PASSWORD": "app_admin",
+			"POSTGRES_DB":       "app_db",
+		},
+
+		WaitingFor: wait.ForAll(
+			wait.ForListeningPort("5432/tcp"),
+			wait.ForLog("database system is ready to accept connections").WithStartupTimeout(60*time.Second),
+		),
+	}
+
+	postgresC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+
+	if err != nil {
+		t.Fatalf("failed to start postgres container: %v", err)
+	}
+
+	// Ensure the container is terminated after the test
+	defer func() { _ = postgresC.Terminate(ctx) }()
+
+	host, err := postgresC.Host(ctx)
+	if err != nil {
+		t.Fatalf("failed to get container host: %v", err)
+	}
+	port, err := postgresC.MappedPort(ctx, "5432")
+	if err != nil {
+		t.Fatalf("failed to get mapped port: %v", err)
+	}
+	dsn := fmt.Sprintf("postgres://app_admin:app_admin@%s:%s/app_db", host, port.Port())
+
+	config, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
 		panic("failed to parse pgx config")
 	}
